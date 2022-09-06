59cf73156a35fa9f2e29c5f7b097d6c197376ced
이도경 <2dokyoung@gmail.com>
2022-09-06 11:28:25 +0900

    headerIcon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,80 @@
 import React from "react";
 import Image from "next/image";
-//Logoimage
-import logoImage from "./images/jjuppLogo.png";
+//Icon
+import LogoIcon from "./images/jjuppLogo.png";
+import DecoIcon from "./images/jjuppDeco.png";
+import interviewIcon from "./images/jjuppInterview.png";
+import ListIcon from "./images/jjuppList.png";
 const Header: React.FC = () => {
   return (
     <div>
       <header>
-        <div style={{ position: "relative", maxWidth: "15%" }}>
+        <div
+          style={{
+            float: "left",
+            width: "15%",
+            position: "relative",
+            maxWidth: "15%",
+          }}
+        >
           <Image
-            src={logoImage}
+            src={LogoIcon}
             alt="jjuppLogo"
             placeholder="blur"
             layout="responsive"
           />
         </div>
+        <div
+          className="Icon"
+          style={{
+            float: "left",
+            width: "15%",
+            position: "relative",
+            maxWidth: "15%",
+          }}
+        >
+          <Image
+            src={interviewIcon}
+            alt="jjuppInterview"
+            placeholder="blur"
+            layout="responsive"
+          />
+        </div>
+        <div
+          className="Icon"
+          style={{
+            float: "left",
+            width: "15%",
+            position: "relative",
+            maxWidth: "15%",
+          }}
+        >
+          <Image
+            src={DecoIcon}
+            alt="jjuppDeco"
+            placeholder="blur"
+            layout="responsive"
+          />
+        </div>
+        <div
+          className="Icon"
+          style={{
+            float: "left",
+            width: "15%",
+            position: "relative",
+            maxWidth: "15%",
+          }}
+        >
+          <Image
+            src={ListIcon}
+            alt="jjuppList"
+            placeholder="blur"
+            layout="responsive"
+          />
+        </div>
       </header>
-      <div id="header-line"></div>
+      <div style={{ clear: "both" }} id="header-upline"></div>
+      <div style={{ clear: "both" }} id="header-line"></div>
     </div>
   );
 };
