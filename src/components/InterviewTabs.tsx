7a0fa3dca48d93a4571ea64bf45cc2f9b101333f
이도경 <2dokyoung@gmail.com>
2022-09-15 01:40:04 +0900

    좆같네

--- src/components/InterviewTabs.tsx
+++ src/components/InterviewTabs.tsx
@@ -6,11 +6,11 @@ import Tab from "react-bootstrap/Tab";
 const InterviewT: React.FC = () => {
   return (
     <div>
-      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-        <Row>
-          <Col sm={3}>
-            <Nav variant="pills" className="flex-column">
-              <Nav.Item>
+      <div id="interviewBox">
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col>
+              <Nav variant="pills" className="flex-column">
                 <Nav.Link eventKey="first">
                   <div>A</div>
                   <div>B</div>
@@ -18,32 +18,28 @@ const InterviewT: React.FC = () => {
                   <div>U</div>
                   <div>T</div>
                 </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
                 <Nav.Link eventKey="second">
                   <div>인</div>
                   <div>터</div>
                   <div>뷰</div>
                 </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
                 <Nav.Link eventKey="third">
                   <div>메</div>
                   <div>모</div>
                   <div>장</div>
                 </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-          <Col sm={9}>
-            <Tab.Content>
-              <Tab.Pane eventKey="first">First</Tab.Pane>
-              <Tab.Pane eventKey="second">Second</Tab.Pane>
-              <Tab.Pane eventKey="third">Third</Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
+              </Nav>
+            </Col>
+            <Col>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">First</Tab.Pane>
+                <Tab.Pane eventKey="second">Second</Tab.Pane>
+                <Tab.Pane eventKey="third">Third</Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </div>
     </div>
   );
 };
