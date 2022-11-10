80d320a058034f8ea59ad4a8a96b5fce9af23ac6
이도경 <2dokyoung@gmail.com>
2022-11-10 10:51:11 +0900

    deploy to gh-pages

---
+++ out/_next/static/HyBUrsKIGA7eVpyEaaxIl/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002FFood\u002F[RestID]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- out/_next/static/WecsBBByedxu7jc0ZVUPF/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002FKor\u002F[RestID]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
